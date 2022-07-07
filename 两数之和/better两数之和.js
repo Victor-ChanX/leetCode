db3c3dd2cf5c4f46f225c8dce668ae71d6c386e0
victor <522537954@qq.com>
2022-07-07 22:31:16 +0800

    fix： 1、写注释

--- 两数之和/better两数之和.js
+++ 两数之和/better两数之和.js
@@ -1,10 +1,14 @@
-var twoSum = function(nums, target) {
-    const map = new Map();
-    for(let i = 0, len = nums.length;i < len;i++) {
-        if(map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i];
-        }
-        map.set(nums[i], i);
-    }
-    return [];
-};
+var twoSum = function (nums, target) {
+	//构筑了一个map
+	const map = new Map()
+	//根据给定的测试数组进行遍历
+	for (let i = 0, len = nums.length; i < len; i++) {
+		//为了减少遍历次数增加效率，如果此时map中已经有了target减去当前队列的num[i]所匹配的结果，直接返回
+		if (map.has(target - nums[i])) {
+			return [map.get(target - nums[i]), i]
+		}
+		//如果没有则继续将二维数组组装到map里面
+		map.set(nums[i], i)
+	}
+	return []
+}
--- 两数之和/两数之和.js
+++ 两数之和/两数之和.js
@@ -1,24 +1,29 @@
+//先声明了一个方法作为处理所有相加的结果
 var sums = function (num1, otherNums) {
-    let result = []
-    for (x in otherNums) {
-        let sum = num1 + otherNums[x]
-        result.push(sum)
-    }
-    return result
+	let result = []
+	for (x in otherNums) {
+		let sum = num1 + otherNums[x]
+		result.push(sum)
+	}
+	return result
 }
 
 var twoSum = function (nums, target) {
-    let answer = []
-    for (let i = 0; i < nums.length; i++) {
-        if (i + 1 <= nums.length) {
-            let result = sums(nums[i], nums.slice(i + 1))
-            console.log(i, result.indexOf(target) + i + 1, 'tesdt')
-            if (result.includes(target)) {
-                answer.push(i, result.indexOf(target) + i + 1)
-                return answer
-            }
-        }
-    }
+	//声明一个待返回结果的空数组
+	let answer = []
+	//根据给定的测试数组进行循环
+	for (let i = 0; i < nums.length; i++) {
+		//判断是否i到达边界
+		if (i + 1 <= nums.length) {
+			//调用方法获得一个请求结果的数组
+			let result = sums(nums[i], nums.slice(i + 1))
+			//利用数组方法判断结果里面是否有符合target一样的值，如果有则将下标取出返回
+			if (result.includes(target)) {
+				answer.push(i, result.indexOf(target) + i + 1)
+				return answer
+			}
+		}
+	}
 }
 
 twoSum([2, 7, 11, 15], 9)
